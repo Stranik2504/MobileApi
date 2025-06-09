f91d0907c25672027ff45485f5da62e27227c7de
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2025-06-09 06:19:25 +0300

    Add JsonPropertyName attributes to User properties for serialization

--- MobileApi/Models/User.cs
+++ MobileApi/Models/User.cs
@@ -4,7 +4,10 @@ namespace MobileApi.Models;
 
 public class User
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
     [JsonPropertyName("name")]
     public string Username { get; set; }
+    [JsonPropertyName("selected")]
+    public bool Selected { get; set; }
 }
